da2e26d10e249eb1c25f21df9a06a63f82586e3b
Mohammad al-duraidi <Mohammad.al-duraidi@tarjama.com>
2023-02-06 21:43:03 +0300

    add config

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+export default {
+MARVEL_CONFIGURATION:{
+    baseUrl: process.env.REACT_APP_MARVEL_BASE_URL || '',
+    publicKey: process.env.REACT_APP_MARVEL_PUBLIC_KEY || '',
+    privateKey: process.env.REACT_APP_MARVEL_PRIVATE_KEY || '',
+}
+}
